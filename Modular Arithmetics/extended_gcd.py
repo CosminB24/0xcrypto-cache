db90375eecffa42fae4efc5d81406595050804c7
CosminB24 <cosminbadea919@yahoo.com>
2026-02-15 18:28:34 +0200

    extended gcd added

---
+++ Modular Arithmetics/extended_gcd.py
@@ -0,0 +1,21 @@
+def computeGCD(x, y):
+  
+   while(y):
+       x, y = y, x % y
+  
+   return x
+# return (u, v) such that au + bv = gcd(a, b)
+def extended_euclidean(a, b): # a > b
+    if a < b:
+        return 0, 0
+    else:
+        if a % b == computeGCD(a, b):
+            return 1, 0 - int(a / b)
+        else:
+            u, v = extended_euclidean(b, a % b)
+            return v, u - v * int(a / b)
+
+p = 26513
+q = 32321
+
+print(extended_euclidean(q,p))
